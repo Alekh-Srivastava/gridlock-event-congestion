4824b3d123ee7781e0e409e5c8b0d6028bfff9dc
Alekh Srivastava <Alekh.Srivastava@esphere.ai>
2026-06-23 15:04:40 +0000

    fixes

--- pages/1_ML_Models.py
+++ pages/1_ML_Models.py
@@ -9,11 +9,6 @@ import pandas as pd
 import numpy as np
 import altair as alt
 
-st.set_page_config(
-    page_title="GridLock · ML Architecture",
-    page_icon="🧠",
-    layout="wide",
-)
 
 ss = st.session_state
 
